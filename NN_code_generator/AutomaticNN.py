9053a9b4f766a01bc291f53a019564fe9a8858e5
Aj! <88217322+ajaverett@users.noreply.github.com>
2023-02-20 23:12:23 -0700

    oops

--- NN_code_generator/AutomaticNN.py
+++ NN_code_generator/AutomaticNN.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import pandas as pd
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-
 
 st.sidebar.title('Neural Network Code Generator')
 st.sidebar.header('Preprocess Data:')
